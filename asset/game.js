9000946b61af3d9d2f5d5f876a72bc314cb05a17
Alexander F. Smith-Bove <afs4@tcl216-16.cs.williams.edu>
2017-01-03 15:04:36 -0500

    adding console

--- asset/game.js
+++ asset/game.js
@@ -1,2 +1,18 @@
-console.log("it worked");
-alert("js loaded!");
+console.log("game.js loaded");
+
+window.onload = function(){
+  console.log("starting WSRL - window loaded");
+  // check if rot.js can work on the browser
+  if(!ROT.isSupported()){
+    alert("the rot.js library isn't supported by your browser");
+  }else{
+    //initialize the game
+    Game.init();
+  }
+};
+
+var Game = {
+  init: function(){
+    console.log("game initialized");
+  }
+};
