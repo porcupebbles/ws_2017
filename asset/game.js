52f1ed8b247b0965ff2ae837864f32894dfdd85e
Alexander F. Smith-Bove <afs4@tcl216-16.cs.williams.edu>
2017-01-03 15:40:28 -0500

    adding console. it works

--- asset/game.js
+++ asset/game.js
@@ -8,11 +8,31 @@ window.onload = function(){
   }else{
     //initialize the game
     Game.init();
+
+    document.getElementById('mh-main-display').appendChild(
+      Game.display.main.o.getContainer());
   }
 };
 
 var Game = {
+  display: {
+    main: {
+      w: 80,
+      h: 24,
+      o: null
+    }
+  },
   init: function(){
     console.log("game initialized");
+    this.display.main.o = new ROT.Display(
+      {width: this.display.main.w,
+       height: this.display.main.h}
+    );
+
+    for (var i = 0; i < Game.display.h; i++){
+      for ( var j = 0; j < Game.display.w; j++){
+        this.display.main.o.draw(6,6,"@", "#Of0");
+      }
+    }
   }
 };
