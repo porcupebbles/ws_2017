6d84db8af1f2edb6751bd1f93be3927a45113df9
Alexander F. Smith-Bove <afs4@tcl216-16.cs.williams.edu>
2017-01-04 14:10:41 -0500

    display all 3 consoles

--- asset/game.js
+++ asset/game.js
@@ -9,30 +9,63 @@ window.onload = function(){
     //initialize the game
     Game.init();
 
+    document.getElementById('mh-avatar-display').appendChild(
+      Game.getDisplay('avatar').getContainer());
     document.getElementById('mh-main-display').appendChild(
-      Game.display.main.o.getContainer());
+      Game.getDisplay('main').getContainer());
+    document.getElementById('mh-message-display').appendChild(
+      Game.getDisplay('message').getContainer());
   }
 };
 
 var Game = {
+  
+  _DISPLAY_SPACING: 1.1,
   display: {
     main: {
       w: 80,
       h: 24,
       o: null
+    },
+    avatar: {
+      w: 20,
+      h: 24,
+      o: null
+    },
+    message: {
+      w: 100,
+      h: 5,
+      o: null
     }
   },
-  init: function(){
-    console.log("game initialized");
-    this.display.main.o = new ROT.Display(
-      {width: this.display.main.w,
-       height: this.display.main.h}
-    );
 
-    for (var i = 0; i < Game.display.h; i++){
-      for ( var j = 0; j < Game.display.w; j++){
-        this.display.main.o.draw(6,6,"@", "#Of0");
+  init: function() {
+      this._randomSeed = 5 + Math.floor(Math.random()*100000);
+      //this._randomSeed = 76250;
+      console.log("using random seed "+this._randomSeed);
+      ROT.RNG.setSeed(this._randomSeed);
+
+      for(var d_key in  this.display){
+        this.display[d_key].o = new ROT.Display({width: this.display[d_key].w,
+          height: this.display[d_key].h, spacing: Game._DISPLAY_SPACING});
+      }
+
+      console.dir(this.display);
+
+      this.renderMain();
+    },
+
+    getDisplay: function (displayId) {
+      if (this.display.hasOwnProperty(displayId)) {
+        return this.display[displayId].o;
+      }
+      return null;
+    },
+
+    renderMain: function() {
+      var d = this.display.main.o;
+      for (var i = 0; i < 10; i++) {
+        d.drawText(5,i+5,"hello world");
       }
     }
-  }
-};
+  };
